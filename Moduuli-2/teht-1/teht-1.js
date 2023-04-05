f01a00d4d0d3553735b26664c7534d8e0da51c9b
arttuvee <arttuvo@metropolia.fi>
2023-04-05 15:19:26 +0300

    Tehtävä 1 tehty

---
+++ Moduuli-2/teht-1/teht-1.js
@@ -0,0 +1,23 @@
+
+// Asks user for 5 numbers and adds them to an array
+function addNumbersToArray() {
+  let numbers = [];
+  for(let i = 0; i < 5; i++) {
+  numbers.push(prompt('Enter a number'));
+  }
+  return numbers
+}
+
+// Reverse and add to new array
+function reverseArray(array) {
+  let numbersReversed = [];
+  for (let i = 0; i < numbers.length; i++) {
+    numbersReversed[i] = numbers[numbers.length - i - 1];
+  }
+  return numbersReversed;
+}
+
+
+
+
+
