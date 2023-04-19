8ea31bf32fcd2b816f467dae6d39bf4bab02e725
arttuvee <arttuvo@metropolia.fi>
2023-04-19 13:09:27 +0300

    teht 4 tehty

---
+++ Moduuli-4/teht-4/teht-4.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const searchForm = document.querySelector('#search-form');
+const resultsDiv = document.querySelector('#results');
+
+searchForm.addEventListener('submit', async function(evt) {
+  evt.preventDefault();
+
+  const code = document.querySelector('input[name=q]').value;
+  try {
+    const response = await fetch(`https://api.tvmaze.com/search/shows?q=${code}`);
+    const shows = await response.json();
+    resultsDiv.innerHTML = ''; // Clear previous results
+
+    shows.forEach((tvShow) => {
+      const article = document.createElement('article');
+      const heading = document.createElement('h2');
+      const name = document.createTextNode(tvShow.show.name);
+      heading.appendChild(name);
+      article.appendChild(heading);
+
+      const link = document.createElement('a');
+      link.href = tvShow.show.url;
+      link.target = '_blank';
+
+      const image = document.createElement('img');
+      image.src = tvShow.show.image ? tvShow.show.image.medium : 'https://via.placeholder.com/210x295?text=Not%20Found';
+      image.alt = tvShow.show.name;
+      link.appendChild(image);
+      article.appendChild(link);
+
+      const summary = document.createElement('div');
+      summary.innerHTML = tvShow.show.summary;
+      article.appendChild(summary);
+      resultsDiv.appendChild(article);
+    });
+
+  } catch (error) {
+    console.log(error.message);
+  }
+});
